da51c3ed4f78b88c12f5e2a154a0addaeb244700
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-06 23:21:01 +0100

    Added and set up toastify

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./fORM.JSX";
 import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const [colorsList, setColorsList] = useState(new Values("#FF0000").all(10));
+
   return (
     <main>
       <Form />
       <ColorList colors={colorsList} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };

445e0fe8c227782a6dd3d6f9289f203c01d91410
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-06 22:10:10 +0100

    Generated Colors using Values js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./fORM.JSX";
+import Values from "values.js";
 
 const App = () => {
+  const [colorsList, setColorsList] = useState(new Values("#FF0000").all(10));
   return (
     <main>
       <Form />
-      <ColorList />
+      <ColorList colors={colorsList} />
     </main>
   );
 };
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,4 +1,10 @@
-const SingleColor = () => {
-  return <div>SingleColor</div>;
+const SingleColor = (props) => {
+  const { hex, weight } = props.color;
+  return (
+    <article className="color" style={{ background: `#${hex}` }}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </article>
+  );
 };
 export default SingleColor;

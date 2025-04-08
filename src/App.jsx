b70755586760eb41451f79ccee05d2a0beaf5118
ProfessorBuny <nikunjraghav24@gmail.com>
2025-04-08 15:16:12 +0100

    updating color values

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { useState } from "react";
-import ColorList from "./ColorList";
-import Form from "./fORM.JSX";
-import Values from "values.js";
-import { ToastContainer, toast } from "react-toastify";
+import { useState } from 'react'
+import ColorList from './ColorList'
+import Form from './fORM.JSX'
+import Values from 'values.js'
+import { ToastContainer, toast } from 'react-toastify'
 
 const App = () => {
-  const [colorsList, setColorsList] = useState(new Values("#FF0000").all(10));
+  const [colorsList, setColorsList] = useState(new Values('#FF0000').all(10))
   const addColor = (color) => {
     try {
-      setColorsList(new Values(color).all(10));
+      const colorValues = new Values(color)
+      const colorList = colorValues.all(10)
+      setColorsList(colorList)
+      // setColorsList(new Values(color).all(10))
     } catch (error) {
-      toast.error(error.message);
+      toast.error(error.message)
     }
-  };
+  }
   return (
     <main>
       <Form addColor={addColor} />
       <ColorList colors={colorsList} />
       <ToastContainer position="top-center" />
     </main>
-  );
-};
-export default App;
+  )
+}
+export default App

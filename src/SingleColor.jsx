190ae5d2a86b37adbd28e00856a86ed964a223be
ProfessorBuny <nikunjraghav24@gmail.com>
2025-04-08 15:26:04 +0100

    setting on click on hex color

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,28 +1,29 @@
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify'
 
 const SingleColor = (props) => {
-  const { hex, weight } = props.color;
+  const { hex, weight } = props.color
   const clickHandler = async () => {
     if (navigator.clipboard) {
       try {
-        await navigator.clipboard.writeText(`#${hex}`);
-        toast.success("Successfully copied to clipboard");
+        await navigator.clipboard.writeText(`#${hex}`)
+        toast.success('Successfully copied to clipboard')
       } catch (error) {
-        toast.error("Failed to copy");
+        toast.error('Failed to copy')
       }
     } else {
-      toast.error("Can't access clipboard");
+      toast.error("Can't access clipboard")
     }
-  };
+  }
   return (
     <article
-      className={props.index > 10 ? "color color-light" : "color"}
+      className={props.index > 10 ? 'color color-light' : 'color'}
       style={{ background: `#${hex}` }}
-      onClick={clickHandler}
     >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">#{hex}</p>
+      <p className="color-value" onClick={clickHandler}>
+        #{hex}
+      </p>
     </article>
-  );
-};
-export default SingleColor;
+  )
+}
+export default SingleColor
